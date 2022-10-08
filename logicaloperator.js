4427f71a6f4e280b9f70af1d77c6f82fa67ea93e
malisiddiquie <110660438+malisiddiquie@users.noreply.github.com>
2022-10-08 16:43:09 +0500

    Add files via upload

---
+++ logicaloperator.js
@@ -0,0 +1,20 @@
+/*
+LOGICAL CO
+|| (OR)
+&& (AND)
+! (NOT)
+*/
+//&& LOGICAL OPERATOR
+ var a = 60;
+ alert("&& operator: ( Operand-1 is true && Operand-2 is true therefore End result comes true");
+ var b = prompt("Put value 1 for Operands" );
+  var c = (a > 50 && a < 70 );
+
+    if (b == 1) {
+        alert("a > 50 && a < 70 = " + c);
+    }
+        else {
+        alert(" only Value 1 was allowed to see Operands.");
+    } 
+    
+ alert("Thanks");
